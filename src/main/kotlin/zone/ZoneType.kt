77f1d4719888bf1bb26127f2d5b149ad3783a171
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2024-03-14 21:10:03 -0700

    ZoneType

---
+++ src/main/kotlin/zone/ZoneType.kt
@@ -0,0 +1,11 @@
+package net.mcquest.engine.zone
+
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
+
+enum class ZoneType(val priority: Int, val color: TextColor) {
+    REGION(0, NamedTextColor.YELLOW),
+    WILDERNESS(1, NamedTextColor.YELLOW),
+    SETTLEMENT(2, NamedTextColor.GREEN),
+    DUNGEON(3, NamedTextColor.GRAY)
+}
