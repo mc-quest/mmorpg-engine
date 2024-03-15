7a92e3c2513979b2bbb2636879c3378c702e0d03
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2024-03-14 19:57:58 -0700

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.22"
     application
 }
 
@@ -10,15 +10,31 @@ repositories {
     mavenCentral()
     maven("https://jitpack.io")
     maven("https://repo.unnamed.team/repository/unnamed-public/")
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
-    implementation("dev.hollowcube:minestom-ce:1554487748")
+    implementation("com.github.minestom:minestom:improved-pathfinding-SNAPSHOT")
+
+    implementation("com.google.guava:guava:33.0.0-jre")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
+
     implementation("team.unnamed:creative-api:1.5.0")
     implementation("team.unnamed:creative-serializer-minecraft:1.5.0")
-    implementation("team.unnamed:hephaestus-api:0.6.0-SNAPSHOT")
-    implementation("team.unnamed:hephaestus-reader-blockbench:0.6.0-SNAPSHOT")
-    implementation("team.unnamed:hephaestus-runtime-minestom-ce:0.6.0-SNAPSHOT")
+
+    implementation("team.unnamed:hephaestus-api:0.9.1-SNAPSHOT")
+    implementation("team.unnamed:hephaestus-reader-blockbench:0.9.1-SNAPSHOT")
+    implementation("team.unnamed:hephaestus-runtime-minestom:0.9.1-SNAPSHOT")
+
+    implementation("net.kyori:adventure-text-minimessage:4.16.0")
+
+    implementation("org.gagravarr:vorbis-java-core:0.8")
+
     testImplementation(kotlin("test"))
 }
 
