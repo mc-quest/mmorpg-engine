72c4f46cdbe1244266e2bf587e06368a6090507f
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2024-05-01 16:26:28 -0700

    LoopForever

---
+++ src/main/kotlin/ai/behavior/decorator/LoopForever.kt
@@ -0,0 +1,25 @@
+package net.mcquest.engine.ai.behavior.decorator
+
+import com.fasterxml.jackson.databind.JsonNode
+import net.mcquest.engine.ai.behavior.*
+import net.mcquest.engine.character.NonPlayerCharacter
+
+class LoopForever(child: Behavior) : Decorator(child) {
+    override fun update(character: NonPlayerCharacter): BehaviorStatus {
+        while (true) {
+            val status = child.tick(character)
+
+            if (status != BehaviorStatus.SUCCESS) {
+                return status
+            }
+        }
+    }
+}
+
+class LoopForeverBlueprint(val child: BehaviorBlueprint) : BehaviorBlueprint() {
+    override fun create() = LoopForever(child.create())
+}
+
+fun deserializeLoopForever(data: JsonNode) = LoopForeverBlueprint(
+    deserializeBehaviorBlueprint(data["child"])
+)
