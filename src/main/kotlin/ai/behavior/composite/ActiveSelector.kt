651e9bbfeb21f462a0a0e2f1da033de49dc741b1
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2024-05-01 15:03:58 -0700

    ActiveSelector

---
+++ src/main/kotlin/ai/behavior/composite/ActiveSelector.kt
@@ -0,0 +1,55 @@
+package net.mcquest.engine.ai.behavior.composite
+
+import com.fasterxml.jackson.databind.JsonNode
+import net.mcquest.engine.ai.behavior.*
+import net.mcquest.engine.character.NonPlayerCharacter
+
+class ActiveSelector(children: List<Behavior>) : Composite(children) {
+    private var currentChild = 0
+
+    override fun start(character: NonPlayerCharacter) {
+        currentChild = 0
+    }
+
+    override fun update(character: NonPlayerCharacter): BehaviorStatus {
+        val prevChild = currentChild
+        currentChild = 0
+
+        var status = BehaviorStatus.FAILURE
+
+        while (currentChild < children.size) {
+            val childStatus = children[currentChild].tick(character)
+
+            if (childStatus != BehaviorStatus.FAILURE) {
+                status = childStatus
+                break
+            }
+
+            currentChild++
+        }
+
+        if (prevChild != children.size && currentChild != prevChild) {
+            children[prevChild].abort(character)
+        }
+
+        return status
+    }
+
+    override fun stop(character: NonPlayerCharacter) {
+        if (currentChild < children.size) {
+            val child = children[currentChild]
+            if (child.status == BehaviorStatus.RUNNING) {
+                child.abort(character)
+            }
+        }
+    }
+}
+
+class ActiveSelectorBlueprint(
+    private val children: List<BehaviorBlueprint>
+) : BehaviorBlueprint() {
+    override fun create() = ActiveSelector(children.map(BehaviorBlueprint::create))
+}
+
+fun deserializeActiveSelectorBlueprint(data: JsonNode) =
+    ActiveSelectorBlueprint(data["children"].map(::deserializeBehaviorBlueprint))
