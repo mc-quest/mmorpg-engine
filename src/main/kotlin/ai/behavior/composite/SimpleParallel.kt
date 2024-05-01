a576ceba5c15eb743beb07025a548e61af90fa5c
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2024-05-01 15:05:14 -0700

    SimpleParallel

---
+++ src/main/kotlin/ai/behavior/composite/SimpleParallel.kt
@@ -0,0 +1,39 @@
+package net.mcquest.engine.ai.behavior.composite
+
+import com.fasterxml.jackson.databind.JsonNode
+import net.mcquest.engine.ai.behavior.*
+import net.mcquest.engine.character.NonPlayerCharacter
+
+class SimpleParallel(
+    primary: Behavior,
+    secondary: Behavior
+) : Composite(listOf(primary, secondary)) {
+    private val primary
+        get() = children[0]
+
+    private val secondary
+        get() = children[1]
+
+    override fun update(character: NonPlayerCharacter): BehaviorStatus {
+        val status = primary.tick(character)
+        secondary.tick(character)
+        return status
+    }
+
+    override fun stop(character: NonPlayerCharacter) {
+        if (primary.status == BehaviorStatus.RUNNING) primary.abort(character)
+        if (secondary.status == BehaviorStatus.RUNNING) secondary.abort(character)
+    }
+}
+
+class SimpleParallelBlueprint(
+    private val primary: BehaviorBlueprint,
+    private val secondary: BehaviorBlueprint
+) : BehaviorBlueprint() {
+    override fun create() = SimpleParallel(primary.create(), secondary.create())
+}
+
+fun deserializeSimpleParallelBlueprint(data: JsonNode) = SimpleParallelBlueprint(
+    deserializeBehaviorBlueprint(data["primary"]),
+    deserializeBehaviorBlueprint(data["secondary"])
+)
