334f1992d4cf4ce318ec703889ec9c3da811e647
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2024-05-02 11:01:27 -0700

    CooldownReady

---
+++ src/main/kotlin/ai/behavior/task/CooldownReady.kt
@@ -0,0 +1,31 @@
+package net.mcquest.engine.ai.behavior.task
+
+import com.fasterxml.jackson.databind.JsonNode
+import net.mcquest.engine.ai.behavior.BehaviorBlueprint
+import net.mcquest.engine.ai.behavior.BehaviorStatus
+import net.mcquest.engine.ai.behavior.Task
+import net.mcquest.engine.character.NonPlayerCharacter
+import net.mcquest.engine.time.Cooldown
+import net.mcquest.engine.time.secondsToMillis
+
+class CooldownReady(durationMillis: Long) : Task() {
+    private val cooldown = Cooldown(durationMillis)
+
+    override fun update(character: NonPlayerCharacter) =
+        if (cooldown.hasCooldown(character.runtime.timeMillis)) {
+            cooldown.set(character.runtime.timeMillis)
+            BehaviorStatus.SUCCESS
+        } else {
+            BehaviorStatus.FAILURE
+        }
+}
+
+class CooldownReadyBlueprint(
+    private val durationMillis: Long
+) : BehaviorBlueprint() {
+    override fun create() = CooldownReady(durationMillis)
+}
+
+fun deserializeCooldownReadyBlueprint(data: JsonNode) = CooldownReadyBlueprint(
+    secondsToMillis(data["duration_seconds"].asDouble())
+)
