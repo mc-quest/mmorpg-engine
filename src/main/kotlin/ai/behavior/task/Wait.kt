1335ab228e28ccdab8398d0ecea42f0fce62be0f
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2024-05-02 11:34:22 -0700

    Wait

---
+++ src/main/kotlin/ai/behavior/task/Wait.kt
@@ -0,0 +1,33 @@
+package net.mcquest.engine.ai.behavior.task
+
+import com.fasterxml.jackson.databind.JsonNode
+import net.mcquest.engine.ai.behavior.BehaviorBlueprint
+import net.mcquest.engine.ai.behavior.BehaviorStatus
+import net.mcquest.engine.ai.behavior.Task
+import net.mcquest.engine.character.NonPlayerCharacter
+import net.mcquest.engine.time.secondsToMillis
+
+class Wait(private val durationMillis: Long) : Task() {
+    private var startTimeMillis = 0L
+
+    override fun start(character: NonPlayerCharacter) {
+        startTimeMillis = character.runtime.timeMillis
+    }
+
+    override fun update(character: NonPlayerCharacter) =
+        if (character.runtime.timeMillis - startTimeMillis >= durationMillis) {
+            BehaviorStatus.SUCCESS
+        } else {
+            BehaviorStatus.RUNNING
+        }
+}
+
+class WaitBlueprint(
+    private val durationMillis: Long
+) : BehaviorBlueprint() {
+    override fun create() = Wait(durationMillis)
+}
+
+fun deserializeWaitBlueprint(data: JsonNode) = WaitBlueprint(
+    secondsToMillis(data["seconds"].asDouble())
+)
