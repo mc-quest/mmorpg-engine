18eca563fb9e202607ca6ad042e8a91c4361ffb4
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2025-04-19 16:11:35 -0700

    Remove old managers

--- src/main/kotlin/character/NonPlayerCharacterManager.kt
+++
@@ -1,9 +0,0 @@
-package net.mcquest.engine.character
-
-class NonPlayerCharacterManager(blueprints: Collection<CharacterBlueprint>) {
-    private val blueprintsById = blueprints.associateBy(CharacterBlueprint::id)
-
-    fun getBlueprint(id: String): CharacterBlueprint {
-        return blueprintsById.getValue(id)
-    }
-}
