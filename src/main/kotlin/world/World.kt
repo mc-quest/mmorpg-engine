51321067e1e47ea17b7caaa666cf327b92f68ce7
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2025-04-19 16:15:51 -0700

    World ID to path conversion

--- src/main/kotlin/world/World.kt
+++ src/main/kotlin/world/World.kt
@@ -3,3 +3,5 @@ package net.mcquest.engine.world
 import net.mcquest.engine.resource.parseId
 
 fun parseWorldId(id: String) = parseId(id, "worlds")
+
+fun worldIdToWorldPath(id: String) = id.replace(".", "/")
