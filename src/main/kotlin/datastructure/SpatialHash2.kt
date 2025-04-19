c0f87e4a48f591f150cd97f21930ead4b243e814
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2025-04-19 15:53:22 -0700

    Spatial hashes

--- src/main/kotlin/datastructure/SpatialHash2.kt
+++ src/main/kotlin/datastructure/SpatialHash2.kt
@@ -2,107 +2,51 @@ package net.mcquest.engine.datastructure
 
 import com.google.common.collect.HashMultimap
 import com.google.common.collect.SetMultimap
-import net.mcquest.engine.instance.Instance
-import net.mcquest.engine.math.BoundingBox2
 import net.mcquest.engine.math.Vector2
+import net.mcquest.engine.math.Vector2Int
 import kotlin.math.floor
 
 class SpatialHash2<T>(private val cellSize: Double) {
-    private val valuesByCell: SetMultimap<Cell2, T> = HashMultimap.create()
+    private val grid: SetMultimap<Vector2Int, T> = HashMultimap.create()
 
     val values: Collection<T>
-        get() = valuesByCell.values().toSet()
+        get() = grid.values().toSet()
 
-    fun query(instance: Instance, position: Vector2): Collection<T> =
-        valuesByCell.get(cell(instance, position, cellSize))
+    fun query(position: Vector2): Collection<T> = grid[cell(position)].toSet()
 
-    fun query(instance: Instance, min: Vector2, max: Vector2): Collection<T> =
-        cells(instance, min, max, cellSize).flatMap(valuesByCell::get).toSet()
+    fun query(min: Vector2, max: Vector2): Collection<T> =
+        cells(min, max).flatMap(grid::get).toSet()
 
-    fun query(instance: Instance, center: Vector2, radius: Double) = query(
-        instance,
-        center - Vector2(radius, radius),
-        center + Vector2(radius, radius)
+    fun query(center: Vector2, radius: Double) = query(
+        center - Vector2.ONE * radius,
+        center + Vector2.ONE * radius
     )
 
-    fun put(instance: Instance, position: Vector2, value: T) {
-        valuesByCell.put(cell(instance, position, cellSize), value)
-    }
+    fun put(position: Vector2, value: T) = grid.put(cell(position), value)
 
-    fun put(instance: Instance, min: Vector2, max: Vector2, value: T) =
-        cells(instance, min, max, cellSize).forEach { cell ->
-            valuesByCell.put(cell, value)
-        }
+    fun put(min: Vector2, max: Vector2, value: T) =
+        cells(min, max).forEach { grid.put(it, value) }
 
-    fun put(instance: Instance, boundingBox: BoundingBox2, value: T) =
-        put(instance, boundingBox.min, boundingBox.max, value)
+    fun remove(position: Vector2, value: T) = grid.remove(cell(position), value)
 
-    fun move(
-        oldInstance: Instance,
-        oldPosition: Vector2,
-        newInstance: Instance,
-        newPosition: Vector2,
-        value: T
-    ) {
-        valuesByCell.remove(cell(oldInstance, oldPosition, cellSize), value)
-        valuesByCell.put(cell(newInstance, newPosition, cellSize), value)
-    }
+    fun remove(min: Vector2, max: Vector2, value: T) =
+        cells(min, max).forEach { grid.remove(it, value) }
 
-    fun move(
-        oldInstance: Instance,
-        oldMin: Vector2,
-        oldMax: Vector2,
-        newInstance: Instance,
-        newMin: Vector2,
-        newMax: Vector2,
-        value: T
-    ) {
-        cells(oldInstance, oldMin, oldMax, cellSize).forEach { cell ->
-            valuesByCell.remove(cell, value)
-        }
-        cells(newInstance, newMin, newMax, cellSize).forEach { cell ->
-            valuesByCell.put(cell, value)
-        }
-    }
+    private fun cellCoord(coord: Double) = floor(coord / cellSize).toInt()
 
-    fun remove(instance: Instance, position: Vector2, value: T) {
-        valuesByCell.remove(cell(instance, position, cellSize), value)
-    }
+    private fun cell(position: Vector2) = Vector2Int(
+        cellCoord(position.x),
+        cellCoord(position.y)
+    )
 
-    fun remove(instance: Instance, min: Vector2, max: Vector2, value: T) =
-        cells(instance, min, max, cellSize).forEach { cell ->
-            valuesByCell.remove(cell, value)
-        }
-}
+    private fun cells(min: Vector2, max: Vector2) = sequence {
+        val minCell = cell(min)
+        val maxCell = cell(max)
 
-private data class Cell2(
-    val instance: Instance,
-    val x: Int,
-    val y: Int
-)
-
-private fun cell(
-    instance: Instance,
-    position: Vector2,
-    cellSize: Double
-) = Cell2(
-    instance,
-    floor(position.x / cellSize).toInt(),
-    floor(position.y / cellSize).toInt()
-)
-
-private fun cells(
-    instance: Instance,
-    min: Vector2,
-    max: Vector2,
-    cellSize: Double
-) = sequence {
-    val minCell = cell(instance, min, cellSize)
-    val maxCell = cell(instance, max, cellSize)
-
-    for (x in minCell.x..maxCell.x) {
-        for (y in minCell.y..maxCell.y) {
-            yield(Cell2(instance, x, y))
+        for (x in minCell.x..maxCell.x) {
+            for (y in minCell.y..maxCell.y) {
+                yield(Vector2Int(x, y))
+            }
         }
     }
 }
--- src/main/kotlin/datastructure/SpatialHash3.kt
+++ src/main/kotlin/datastructure/SpatialHash3.kt
@@ -2,103 +2,53 @@ package net.mcquest.engine.datastructure
 
 import com.google.common.collect.HashMultimap
 import com.google.common.collect.SetMultimap
-import net.mcquest.engine.instance.Instance
 import net.mcquest.engine.math.Vector3
+import net.mcquest.engine.math.Vector3Int
 import kotlin.math.floor
 
 class SpatialHash3<T>(private val cellSize: Double) {
-    private val valuesByCell: SetMultimap<Cell3, T> = HashMultimap.create()
+    private val grid: SetMultimap<Vector3Int, T> = HashMultimap.create()
 
     val values: Collection<T>
-        get() = valuesByCell.values().toSet()
+        get() = grid.values().toSet()
 
-    fun query(instance: Instance, min: Vector3, max: Vector3): Collection<T> =
-        cells(instance, min, max, cellSize).flatMap(valuesByCell::get).toSet()
+    fun query(position: Vector3): Collection<T> = grid[cell(position)].toSet()
 
-    fun query(instance: Instance, center: Vector3, radius: Double) = query(
-        instance,
-        center - Vector3(radius, radius, radius),
-        center + Vector3(radius, radius, radius)
+    fun query(min: Vector3, max: Vector3): Collection<T> =
+        cells(min, max).flatMap(grid::get).toSet()
+
+    fun query(center: Vector3, radius: Double) = query(
+        center - Vector3.ONE * radius,
+        center + Vector3.ONE * radius
     )
 
-    fun put(instance: Instance, position: Vector3, value: T) {
-        valuesByCell.put(cell(instance, position, cellSize), value)
-    }
+    fun put(position: Vector3, value: T) = grid.put(cell(position), value)
 
-    fun put(instance: Instance, min: Vector3, max: Vector3, value: T) =
-        cells(instance, min, max, cellSize).forEach { cell ->
-            valuesByCell.put(cell, value)
-        }
+    fun put(min: Vector3, max: Vector3, value: T) =
+        cells(min, max).forEach { grid.put(it, value) }
 
-    fun move(
-        oldInstance: Instance,
-        oldPosition: Vector3,
-        newInstance: Instance,
-        newPosition: Vector3,
-        value: T
-    ) {
-        valuesByCell.remove(cell(oldInstance, oldPosition, cellSize), value)
-        valuesByCell.put(cell(newInstance, newPosition, cellSize), value)
-    }
+    fun remove(position: Vector3, value: T) = grid.remove(cell(position), value)
 
-    fun move(
-        oldInstance: Instance,
-        oldMin: Vector3,
-        oldMax: Vector3,
-        newInstance: Instance,
-        newMin: Vector3,
-        newMax: Vector3,
-        value: T
-    ) {
-        cells(oldInstance, oldMin, oldMax, cellSize).forEach { cell ->
-            valuesByCell.remove(cell, value)
-        }
-        cells(newInstance, newMin, newMax, cellSize).forEach { cell ->
-            valuesByCell.put(cell, value)
-        }
-    }
+    fun remove(min: Vector3, max: Vector3, value: T) =
+        cells(min, max).forEach { grid.remove(it, value) }
 
-    fun remove(instance: Instance, position: Vector3, value: T) {
-        valuesByCell.remove(cell(instance, position, cellSize), value)
-    }
+    private fun cellCoord(coord: Double) = floor(coord / cellSize).toInt()
 
-    fun remove(instance: Instance, min: Vector3, max: Vector3, value: T) =
-        cells(instance, min, max, cellSize).forEach { cell ->
-            valuesByCell.remove(cell, value)
-        }
-}
+    private fun cell(position: Vector3) = Vector3Int(
+        cellCoord(position.x),
+        cellCoord(position.y),
+        cellCoord(position.z)
+    )
 
-private data class Cell3(
-    val instance: Instance,
-    val x: Int,
-    val y: Int,
-    val z: Int
-)
+    private fun cells(min: Vector3, max: Vector3) = sequence {
+        val minCell = cell(min)
+        val maxCell = cell(max)
 
-private fun cell(
-    instance: Instance,
-    position: Vector3,
-    cellSize: Double
-) = Cell3(
-    instance,
-    floor(position.x / cellSize).toInt(),
-    floor(position.y / cellSize).toInt(),
-    floor(position.z / cellSize).toInt(),
-)
-
-private fun cells(
-    instance: Instance,
-    min: Vector3,
-    max: Vector3,
-    cellSize: Double
-) = sequence {
-    val minCell = cell(instance, min, cellSize)
-    val maxCell = cell(instance, max, cellSize)
-
-    for (x in minCell.x..maxCell.x) {
-        for (y in minCell.y..maxCell.y) {
-            for (z in minCell.z..maxCell.z) {
-                yield(Cell3(instance, x, y, z))
+        for (x in minCell.x..maxCell.x) {
+            for (y in minCell.y..maxCell.y) {
+                for (z in minCell.z..maxCell.z) {
+                    yield(Vector3Int(x, y, z))
+                }
             }
         }
     }
