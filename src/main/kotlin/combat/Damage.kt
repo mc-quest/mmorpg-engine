fe1f724842304b3a9413bdb60792db1b50ba97f6
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2025-04-19 19:51:13 -0700

    Damage

--- src/main/kotlin/combat/Damage.kt
+++ src/main/kotlin/combat/Damage.kt
@@ -2,13 +2,25 @@ package net.mcquest.engine.combat
 
 import com.fasterxml.jackson.databind.JsonNode
 
-class Damage(val type: DamageType, val amount: Double)
+class Damage(val damage: Map<DamageType, Double>)
 
 enum class DamageType {
-    PHYSICAL
+    PHYSICAL,
+    ARCANE,
+    FIRE,
+    FROST,
+    NATURE,
+    SHADOW,
+    HOLY
 }
 
-fun deserializeDamage(data: JsonNode) = Damage(
-    DamageType.valueOf(data["type"].asText().uppercase()),
-    data["amount"].asDouble()
-)
+fun deserializeDamage(data: JsonNode): Damage =
+    if (data.isNumber) {
+        Damage(mapOf(DamageType.PHYSICAL to data.asDouble()))
+    } else if (data.isObject) {
+        Damage(data.fields().asSequence().associate { (type, amount) ->
+            DamageType.valueOf(type.uppercase()) to amount.asDouble()
+        })
+    } else {
+        throw IllegalArgumentException()
+    }
