07d1f10efbee10e037b0941783ca7c0289c4dd9e
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2025-04-20 11:04:19 -0700

    Update spatial hashes

--- src/main/kotlin/datastructure/SpatialHash2.kt
+++ src/main/kotlin/datastructure/SpatialHash2.kt
@@ -2,6 +2,7 @@ package net.mcquest.engine.datastructure
 
 import com.google.common.collect.HashMultimap
 import com.google.common.collect.SetMultimap
+import net.mcquest.engine.math.BoundingBox2
 import net.mcquest.engine.math.Vector2
 import net.mcquest.engine.math.Vector2Int
 import kotlin.math.floor
@@ -14,23 +15,18 @@ class SpatialHash2<T>(private val cellSize: Double) {
 
     fun query(position: Vector2): Collection<T> = grid[cell(position)].toSet()
 
-    fun query(min: Vector2, max: Vector2): Collection<T> =
-        cells(min, max).flatMap(grid::get).toSet()
-
-    fun query(center: Vector2, radius: Double) = query(
-        center - Vector2.ONE * radius,
-        center + Vector2.ONE * radius
-    )
+    fun query(boundingBox: BoundingBox2): Collection<T> =
+        cells(boundingBox).flatMap(grid::get).toSet()
 
     fun put(position: Vector2, value: T) = grid.put(cell(position), value)
 
-    fun put(min: Vector2, max: Vector2, value: T) =
-        cells(min, max).forEach { grid.put(it, value) }
+    fun put(boundingBox: BoundingBox2, value: T) =
+        cells(boundingBox).forEach { grid.put(it, value) }
 
     fun remove(position: Vector2, value: T) = grid.remove(cell(position), value)
 
-    fun remove(min: Vector2, max: Vector2, value: T) =
-        cells(min, max).forEach { grid.remove(it, value) }
+    fun remove(boundingBox: BoundingBox2, value: T) =
+        cells(boundingBox).forEach { grid.remove(it, value) }
 
     private fun cellCoord(coord: Double) = floor(coord / cellSize).toInt()
 
@@ -39,9 +35,9 @@ class SpatialHash2<T>(private val cellSize: Double) {
         cellCoord(position.y)
     )
 
-    private fun cells(min: Vector2, max: Vector2) = sequence {
-        val minCell = cell(min)
-        val maxCell = cell(max)
+    private fun cells(boundingBox: BoundingBox2) = sequence {
+        val minCell = cell(boundingBox.min)
+        val maxCell = cell(boundingBox.max)
 
         for (x in minCell.x..maxCell.x) {
             for (y in minCell.y..maxCell.y) {
--- src/main/kotlin/datastructure/SpatialHash3.kt
+++ src/main/kotlin/datastructure/SpatialHash3.kt
@@ -2,6 +2,7 @@ package net.mcquest.engine.datastructure
 
 import com.google.common.collect.HashMultimap
 import com.google.common.collect.SetMultimap
+import net.mcquest.engine.math.BoundingBox3
 import net.mcquest.engine.math.Vector3
 import net.mcquest.engine.math.Vector3Int
 import kotlin.math.floor
@@ -14,23 +15,18 @@ class SpatialHash3<T>(private val cellSize: Double) {
 
     fun query(position: Vector3): Collection<T> = grid[cell(position)].toSet()
 
-    fun query(min: Vector3, max: Vector3): Collection<T> =
-        cells(min, max).flatMap(grid::get).toSet()
-
-    fun query(center: Vector3, radius: Double) = query(
-        center - Vector3.ONE * radius,
-        center + Vector3.ONE * radius
-    )
+    fun query(boundingBox: BoundingBox3): Collection<T> =
+        cells(boundingBox).flatMap(grid::get).toSet()
 
     fun put(position: Vector3, value: T) = grid.put(cell(position), value)
 
-    fun put(min: Vector3, max: Vector3, value: T) =
-        cells(min, max).forEach { grid.put(it, value) }
+    fun put(boundingBox: BoundingBox3, value: T) =
+        cells(boundingBox).forEach { grid.put(it, value) }
 
     fun remove(position: Vector3, value: T) = grid.remove(cell(position), value)
 
-    fun remove(min: Vector3, max: Vector3, value: T) =
-        cells(min, max).forEach { grid.remove(it, value) }
+    fun remove(boundingBox: BoundingBox3, value: T) =
+        cells(boundingBox).forEach { grid.remove(it, value) }
 
     private fun cellCoord(coord: Double) = floor(coord / cellSize).toInt()
 
@@ -40,9 +36,9 @@ class SpatialHash3<T>(private val cellSize: Double) {
         cellCoord(position.z)
     )
 
-    private fun cells(min: Vector3, max: Vector3) = sequence {
-        val minCell = cell(min)
-        val maxCell = cell(max)
+    private fun cells(boundingBox: BoundingBox3) = sequence {
+        val minCell = cell(boundingBox.min)
+        val maxCell = cell(boundingBox.max)
 
         for (x in minCell.x..maxCell.x) {
             for (y in minCell.y..maxCell.y) {
