99d3de27a66caa193c840f6c001e1c68ba69b0e8
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2025-04-19 20:22:36 -0700

    Zones

--- src/main/kotlin/zone/Zone.kt
+++ src/main/kotlin/zone/Zone.kt
@@ -5,10 +5,8 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.mcquest.engine.character.CharacterBlueprint
 import net.mcquest.engine.character.PlayerCharacter
-import net.mcquest.engine.character.deserializeNonPlayerCharacterSpawner
+import net.mcquest.engine.character.deserializeNonPlayerCharacterSpawners
 import net.mcquest.engine.gameobject.GameObjectSpawner
-import net.mcquest.engine.instance.Instance
-import net.mcquest.engine.instance.parseInstanceId
 import net.mcquest.engine.loot.LootChestSpawner
 import net.mcquest.engine.map.AreaMap
 import net.mcquest.engine.map.parseMapId
@@ -17,20 +15,20 @@ import net.mcquest.engine.math.deserializePolygon
 import net.mcquest.engine.math.deserializePosition
 import net.mcquest.engine.music.Song
 import net.mcquest.engine.music.parseSongId
+import net.mcquest.engine.resource.parseId
 import net.mcquest.engine.transition.deserializeTransitionSpawner
 import net.mcquest.engine.util.schedulerManager
-import net.minestom.server.instance.Weather as MinestomWeather
 import java.time.Duration
+import net.minestom.server.instance.Weather as MinestomWeather
 
 class Zone(
     val id: String,
     val name: String,
-    val instance: Instance,
     val type: ZoneType,
     val level: Int,
     val boundary: Polygon,
     val map: AreaMap,
-    val music: Song,
+    val music: Song?,
     val weatherCycle: WeatherCycle
 ) {
     val outerBoundary = boundary.offset(10.0) // TOOD: update offset
@@ -43,6 +41,7 @@ class Zone(
 
     val playerCharacters: Set<PlayerCharacter> = setOf()
 
+    @Suppress("UnstableApiUsage")
     var weather =
         if (weatherCycle.weatherEntries.isEmpty()) Weather.CLEAR
         else weatherCycle.weatherEntries[0].weather
@@ -72,26 +71,16 @@ class Zone(
 fun deserializeZone(
     id: String,
     data: JsonNode,
-    instancesById: Map<String, Instance>,
     mapsById: Map<String, AreaMap>,
     musicById: Map<String, Song>,
     characterBlueprintsById: Map<String, CharacterBlueprint>
 ): Pair<Zone, Collection<GameObjectSpawner>> {
-    val instanceId = parseInstanceId(data["instance"].asText())
-    val instance = instancesById.getValue(instanceId)
-
     val respawnPoints = data["respawn_points"]?.map(::deserializePosition) ?: emptyList()
 
-    val transitionSpawners = data["transitions"]?.map {
-        deserializeTransitionSpawner(it, instance, instancesById)
-    } ?: emptyList()
+    val transitionSpawners = data["transitions"]?.map(::deserializeTransitionSpawner) ?: emptyList()
 
-    val characterSpawners = data["characters"]?.map {
-        deserializeNonPlayerCharacterSpawner(
-            it,
-            instance,
-            characterBlueprintsById
-        )
+    val characterSpawners = data["characters"]?.flatMap {
+        deserializeNonPlayerCharacterSpawners(it, characterBlueprintsById)
     } ?: emptyList()
 
     val lootChestSpawners = listOf<LootChestSpawner>() // TODO
@@ -101,14 +90,15 @@ fun deserializeZone(
     val zone = Zone(
         id,
         data["name"].asText(),
-        instance,
         ZoneType.valueOf(data["type"].asText().uppercase()),
         data["level"].asInt(),
         deserializePolygon(data["boundary"]),
         mapsById.getValue(parseMapId(data["map"].asText())),
-        musicById.getValue(parseSongId(data["music"].asText())),
+        data["music"]?.let { musicById.getValue(parseSongId(it.asText())) },
         data["weather_cycle"]?.let(::deserializeWeatherCycle) ?: WeatherCycle(listOf())
     )
 
     return Pair(zone, spawners)
 }
+
+fun parseZoneId(id: String) = parseId(id, "zones")
